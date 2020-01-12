e68cee2e6f86aedfa37ea4d667d20d2e80ed5d69
MaximDzhezhelo <mdzhezhelo@gmail.com>
2020-01-12 17:25:40 +0200

    made simple pages;

--- src/main/java/ua/kiev/makson/springthymeleafexample/controller/HomeController.java
+++ src/main/java/ua/kiev/makson/springthymeleafexample/controller/HomeController.java
@@ -1,10 +1,18 @@
 package ua.kiev.makson.springthymeleafexample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 
-
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String getExampleHTML(Model model) {
+        model.addAttribute("title", "Win");
+        model.addAttribute("description", "<strong>Thymeleaf</strong> tutorial");
+        return "Home.html";
+    }
 
 }
