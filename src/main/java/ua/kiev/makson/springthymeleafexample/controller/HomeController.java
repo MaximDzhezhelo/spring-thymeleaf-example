67c5207d4cabdbc08b0b1de7fa6799ba1c56991a
MaximDzhezhelo <mdzhezhelo@gmail.com>
2020-01-12 16:23:01 +0200

    make project;

---
+++ src/main/java/ua/kiev/makson/springthymeleafexample/controller/HomeController.java
@@ -0,0 +1,10 @@
+package ua.kiev.makson.springthymeleafexample.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+
+
+}
---
+++ src/test/java/ua/kiev/makson/springthymeleafexample/AppTests.java
@@ -0,0 +1,13 @@
+package ua.kiev.makson.springthymeleafexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
